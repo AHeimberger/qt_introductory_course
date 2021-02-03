2035a7acbba2f298c6652a32e093044f6cbb3489
aheimberger <andreasheimberger@gmx.at>
2021-02-03 10:57:24 +0100

    Add entrypoint file for docker and local build.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,20 +5,34 @@ set -e # exit on any failure (e.g. a crashing test)
 set -o pipefail # propagate a failure through pipe
 
 DIR_CURRENT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# DIR_DEPLOY will be set in Dockerfile
 
 if [ -z ${1} ] || [ ${1} == "help" ] ; then
-    echo "Prints this help information:
+    echo "Help Information:
  - /bin/bash
  - cmake
 "
 elif [ ${#} == 1 ] && [ ${1} == "cmake" ]; then
-    cd "../deploy"
-    cmake "../source/weather_application"
+    if [ ! -z "${DIR_DEPLOY}" ]; then
+        echo "Docker Execution"
+        echo "DIR_DEPLOY: \"${DIR_DEPLOY}\""
+        cd "${DIR_DEPLOY}"
+        cmake "/source/"
+    else
+        echo "Local Execution"
+        export CMAKE_PREFIX_PATH=/opt/Qt/5.15.1/gcc_64
+        export QT_DIR=/opt/Qt/5.15.1/gcc_64/lib/cmake/Qt5
+        rm -rf "deploy"
+        mkdir "deploy"
+        cd "deploy"
+        cmake ..
+    fi
+
     make
+    
     # make tests
-    ./libraries/businesslogic/tests/tests_businesslogic
-    ./libraries/openweathermap/tests/tests_openweathermap
-    # /bin/bash
+    # ./libraries/businesslogic/tests/tests_businesslogic
+    # ./libraries/openweathermap/tests/tests_openweathermap
 elif [ ${#} == 1 ] && [ -e ${1} ]; then
     ${1}
 else
